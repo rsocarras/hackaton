78019a121bd5dfecc0b65fe826fd9f2311f1919b
Rafael Socarras <rafael.socarras@gmail.com>
2013-06-19 03:09:01 -0500

    add all models and controllers by datatlabe

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -33,7 +33,7 @@
     ),
 )); ?>
 
-<div class="container" id="page">
+<div class="container" id="page">	
 
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
